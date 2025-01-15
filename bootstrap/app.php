926913b1a1a8c3597fb12366c59b3c2b6d06b17f
Indrajaya Shinga <putuindrajaya.s.w.t.k@gmail.com>
2025-01-15 20:44:52 +0800

    SITUATION: - tried to expose the project to the internet using ngrok for conecting to midtrans PROBLEM: - everything looks working EXEPT connecting the midtrans - Style off the web not properly loaded FIX: - force the laravel to use https in app.php and made new middleware called UpgradeToHttpsUnderNgrok, and do npm run build and run the php artisan server NOTES: - this accure because the web expect to be using/get respond in https but it doesnt give it iven tho using ngrok, solutin is to force to use https respons so it works" - when runging project, dont need to run npm run dev, npm run build and php artisan serve is enough for the live version - for development, need to use php artisan serve and npm run dev, after finished, need to do npm run build to finalized like the above

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use App\Http\Middleware\UpgradeToHttpsUnderNgrok; // Ensure the middleware is imported
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -12,7 +13,11 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->web([
+            // Add your middleware here
+            UpgradeToHttpsUnderNgrok::class,
+        ]);
+        // You can also configure other middleware stacks if needed
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
